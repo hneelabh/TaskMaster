aa04099ccaf5c340d619edd1167b65fe36a80c81
hneelabh <hneelabh13@gmail.com>
2024-07-19 22:27:26 +0530

    Initial Commit

---
+++ create_db.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+with app.app_context():
+    print("Current working directory:", os.getcwd())
+    print("Database URI:", app.config['SQLALCHEMY_DATABASE_URI'])
+    db.create_all()
+    print("Database created successfully.")
